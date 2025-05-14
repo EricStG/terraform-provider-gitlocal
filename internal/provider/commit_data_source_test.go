dfe569b475f390f38cfd1352f77413537b5218de
Eric St-Georges <gh@thevedge.net>
2025-05-14 09:54:25 -0400

    Use the right commit ID in tests

--- internal/provider/commit_data_source_test.go
+++ internal/provider/commit_data_source_test.go
@@ -16,7 +16,7 @@ func TestCommitDataSource(t *testing.T) {
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: providerConfig + `data "gitlocal_commit" "test" { hash = "13981df24be0f47a94044d21bacdbc3d9132f162" }`,
+				Config: providerConfig + `data "gitlocal_commit" "test" { hash = "239be323657b89192d06e3c97be653e21cf7bff0" }`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrWith("data.gitlocal_commit.test", "date", func(value string) error {
 						expected, _ := time.Parse(time.RFC3339, "2025-05-14T09:08:28-04:00")
